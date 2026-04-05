e962f5461aed4615bf1e831077c01e20f489474c
charitra10 <charitrajain100@gmail.com>
2026-04-05 21:20:22 +0530

    starting move function now

--- Own_code/helper.py
+++ Own_code/helper.py
@@ -12,8 +12,19 @@ class Player:
         self.map_width: int | None=None
         self.map_height: int |None=None
         self.occupied_titanium: list[Position]|None=None
-        self.builder_bot_titanium_search: bool|None=None
-        self.builder_bot_titnium_search_pos: Position |None=None
+        self.pos_markers_placed: list[Position] | None = None
+        self.directions = [
+            ct.Direction.SOUTHEAST
+            ct.DIrection.SOUTHWEST,
+            ct.Direction.NORTHEAST,
+            ct.Direction.NORTHWEST,
+            ct.Direction.SOUTH,
+            ct.Direction.WEST,
+            ct.Direction.NORTH,
+            ct.Direction.EAST,
+
+        ]
+        self.titanium_currently_hunting_for: Position | None = None
 
 
 
@@ -52,37 +63,60 @@ class Player:
             if ct.can_spawn(Position((self.core_pos.x),(self.core_pos.y)-1)):
                 ct.spawn_builder((Position(self.core_pos.x),(self.core_pos.y)-1))
                 self.builder_bots_spawned+=1      
-        
-        #Mid Phase Code
+        #mid phase shenanigans
+
     def run_builder_bot(self,ct:controller)->None:
+
+        # Just finds our own cores position
         if self.core_pos == None:
             for entity_id in ct.get_nearby_entities(1):
                 if ct.get_entity_type(entity_id) == EntityType.CORE:
                    self.core_pos = ct.get_position(entity_id)
+
+        # function call to place marker after reaching the titanium ore
+        if self.titanium_currently_hunting_for == ct.get_position():
+            self.place_marker(self.titanium_currently_hunting_for)
         
+        # if we are currently hunting for an ore then to first move towards it 
+        if self.titanium_currently_hunting_for != None:
+            self.move(self.titanium_currently_hunting_for)
+        
+        # code to find titanium ores already being hunted for
         self.nearby_tiles = ct.get_nearby_tiles()
         for i in self.nearby_tiles:
-            if ct.get_entity_type(ct.get_tile_building_id(i))==EntityType.MARKER:
-                self.occupied_titanium.append(self.marker_position_decode(i))
+            if ct.get_entity_type(ct.get_tile_building_id(i))==EntityType.MARKER and i not in self.pos_markers_placed:
+                if(self.marker_position_decode(ct,i)==self.titanium_currently_hunting_for):
+                    self.titanium_currently_hunting_for= None
+                self.occupied_titanium.append(self.marker_position_decode(ct,i))
+
                 #only for titanium as of now
 
-        if self.builder_bot_titanium_search==False:
+        #finding titanium ore if not hunting for one currently
+        if self.titanium_currently_hunting_for==None:
             for i in self.nearby_tiles:       
                 if ct.get_tile_env(i)==Environement.ORE_TITANIUM and i not in self.occupied_titanium:
-                    self.builder_bot_titanium_search=True
-                    self.builder_bot_titnium_search_pos=i
-                    
-                    self.place_marker(i,ct.get_position())
+                    self.titanium_currently_hunting_for=i
                     self.move(i)
                     break
             
     #Gives the coordinate of the position on the marker
-    def marker_position_decode(self,ct:controller)->Position:
+    def marker_position_decode(self,ct:controller,i:Position)->Position:
         self.val=ct.get_marker_value(ct.get_tile_building_id(i))
         self.y=(val/self.map_width)
         self.x=(val%(self.map_width))
         return Position(self.x,self.y)
 
+    #code to place marker 
+    def place_marker(self,curr_pos:Position,i:Position,ct:controller)->None:
+        self.marker_val = curr_pos.x + curr_pos.y*self.map_width
+        for d in self.directions:
+            if ct.is_tile_empty(i.add(d)):
+                if ct.can_place_marker(i.add(d)):
+                    ct.place_marker(i.add(d),self.marker_val)
+                    self.pos_markers_placed.append(i.add(d))
+                    break
+
+    
 
 
 
